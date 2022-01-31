c80597a61eabbffbcd98ddc79137f4740a4b21bd
Nathan Montelli <nathanmontelli@gmail.com>
2022-01-31 03:10:41 -0800

    more styling

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,60 +5,60 @@ const Contact = () => {
   return (
     <Container>
       <h1 id="contactMe" className='contactMe'>Contact Me</h1>
-    <Form
-    className='formEdit'>
-      <Row>
-        <Col>
-          <FormGroup>
-            <Label for="exampleName">
-              Name
-            </Label>
-            <Input
-              id="exampleName"
-              name="name"
-              type="name"
-            />
-          </FormGroup>
-        </Col>
-        <Col>
-          <FormGroup>
-            <Label for="exampleEmail">
-              Email
-            </Label>
-            <Input
-              id="exampleEmail"
-              name="email"
-              type="email"
-            />
-          </FormGroup>
-        </Col>
-      </Row>
+      <Form
+        className='formEdit'>
+        <Row>
+          <Col>
+            <FormGroup>
+              <Label className='labelColor' for="exampleName">
+                Name
+              </Label>
+              <Input
+                id="exampleName"
+                name="name"
+                type="name"
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label className='labelColor' for="exampleEmail">
+                Email
+              </Label>
+              <Input
+                id="exampleEmail"
+                name="email"
+                type="email"
+              />
+            </FormGroup>
+          </Col>
+        </Row>
 
-      <FormGroup>
-        <Label for="exampleSubject">
-          Subject
-        </Label>
-        <Input
-          id="exampleName"
-          name="name"
-          type="name"
-        />
-      </FormGroup>
-      <FormGroup>
-        <Label for="exampleText">
-          Your Message
-        </Label>
-        <Input
-        className='textarea'
-          id="exampleText"
-          name="text"
-          type="textarea"
-        />
-      </FormGroup>
-      <Button>
-        Submit
-      </Button>
-    </Form>
+        <FormGroup>
+          <Label className='labelColor' for="exampleSubject">
+            Subject
+          </Label>
+          <Input
+            id="exampleName"
+            name="name"
+            type="name"
+          />
+        </FormGroup>
+        <FormGroup>
+          <Label className='labelColor' for="exampleText">
+            Your Message
+          </Label>
+          <Input
+            className='textarea'
+            id="exampleText"
+            name="text"
+            type="textarea"
+          />
+        </FormGroup>
+        <Button>
+          Submit
+        </Button>
+      </Form>
     </Container>
   )
 }
